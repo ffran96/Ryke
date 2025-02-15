2a75b46ed1bda5fbf1e93bb15a9af5e4bcb2faa1
Fran <=>
2025-02-15 11:00:42 +0100

    Subida de shorts

--- src/app/Components/Video.jsx
+++ src/app/Components/Video.jsx
@@ -6,10 +6,11 @@ export default function Video({Source, PosterImage}) {
       src={Source}
       type="video/mp4"
       preload="auto"
-      buffered="true"
       poster={PosterImage}
+      autoPlay
+      muted
+      playsInline
       loop
-      controls
       className={`w-[415px] aspect-[9/16] object-cover rounded-[12px] cursor-pointer`}
     />
   );
