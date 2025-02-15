e57698e2b4f933bb8f1e082beaee8f543433f18e
Fran <=>
2025-02-15 02:24:52 +0100

    First Commit

---
+++ src/app/data/images.jsx
@@ -0,0 +1,80 @@
+import Image1 from "../../../public/enrique-ferri-galeria-1.jpg"
+import Image2 from "../../../public/enrique-ferri-galeria-2.jpg"
+import Image3 from "../../../public/enrique-ferri-galeria-3.jpg"
+import Image4 from "../../../public/enrique-ferri-galeria-4.jpg"
+import Image5 from "../../../public/enrique-ferri-galeria-5.jpg"
+import Image6 from "../../../public/enrique-ferri-galeria-6.jpg"
+import Image7 from "../../../public/enrique-ferri-galeria-7.jpg"
+import Image8 from "../../../public/enrique-ferri-galeria-8.jpg"
+
+
+const GalleryImages = [
+  {
+    id: 1,
+    largeURL: "/enrique-ferri-galeria-1.jpg",
+    altThumbnail:"Josselyn Castro - Actriz",
+    thumbnailURL: Image1,
+    width: 3456,
+    height: 5184,
+  },
+  {
+    id: 2,
+    largeURL: "/enrique-ferri-galeria-2.jpg",
+    altThumbnail: "Josselyn Castro - Actriz",
+    thumbnailURL: Image2,
+    width: 5184,
+    height: 3456,
+  },
+  {
+    id: 3,
+    largeURL: "/enrique-ferri-galeria-3.jpg",
+    altThumbnail: "Josselyn Castro - Actriz",
+    thumbnailURL: Image3,
+    width: 5184,
+    height: 3456,
+  },
+  {
+    id: 4,
+    largeURL: "/enrique-ferri-galeria-4.jpg",
+    altThumbnail: "Josselyn Castro - Actriz",
+    thumbnailURL: Image4,
+    width: 5184,
+    height: 3456,
+  },
+  {
+    id: 5,
+    largeURL: "/enrique-ferri-galeria-5.jpg",
+    altThumbnail: "Josselyn Castro - Actriz",
+    thumbnailURL: Image5,
+    width: 5184,
+    height: 3456,
+  },
+  {
+    id: 6,
+    largeURL: "/enrique-ferri-galeria-6.jpg",
+    altThumbnail: "Josselyn Castro - Actriz",
+    thumbnailURL: Image6,
+    width: 5184,
+    height: 3456,
+  },
+  {
+    id: 7,
+    largeURL: "/enrique-ferri-galeria-7.jpg",
+    altThumbnail: "Josselyn Castro - Actriz",
+    thumbnailURL: Image7,
+    width: 5184,
+    height: 3456,
+  },
+  {
+    id: 8,
+    largeURL: "/enrique-ferri-galeria-8.jpg",
+    altThumbnail: "Josselyn Castro - Actriz",
+    thumbnailURL: Image8,
+    width: 5184,
+    height: 3456,
+  },
+];
+
+export default GalleryImages;
+
+
