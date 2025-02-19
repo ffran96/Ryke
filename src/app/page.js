db16ba0765beb5bb4edba2082ab850fda8921dde
Fran <=>
2025-02-19 23:04:18 +0100

    Se eliminan lineas no usadas

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,14 @@
 import Hero from "./Components/Hero";
 import Conoceme from "./Components/Conoceme";
-import NewCV from "./Components/NewCV";
 import NewGallery from "./Components/NewGallery";
 import NewVideoBook from "./Components/NewVideoBook";
 import Contacto from "./Components/Contacto";
-import LiteYouTubeEmbed from 'react-lite-youtube-embed';
 import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
 
 export default function Home() {
   return (
     <main className="relative">
-      <Hero wallpaper="/joss-portada.webp" alt="Josselyn Castro Garces" />
+      <Hero />
       <Conoceme />
       <NewVideoBook />
       <NewGallery />
