66466b5ecd2b9a9d7119a23dbfcec9c5986f8fca
Fran <=>
2025-02-23 07:47:10 +0100

    Video hero en slug page

--- src/app/Components/ContentSection.jsx
+++ src/app/Components/ContentSection.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function ContentSection({ children, SectionId }) {
+export default function ContentSection({ children, SectionId, Class }) {
   return (
     <section
       id={SectionId}
-      className="max-w-[365px] sm:max-w-[415px] md:max-w-[600px] lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl mx-auto"
+      className={`${Class} max-w-[365px] sm:max-w-[415px] md:max-w-[600px] lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl mx-auto`}
     >
       {children}
     </section>
--- src/app/ultimos-trabajos/[slug]/page.jsx
+++ src/app/ultimos-trabajos/[slug]/page.jsx
@@ -5,6 +5,7 @@ import LiteYouTubeEmbed from "react-lite-youtube-embed";
 import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 import NewVideoBook from "@/app/Components/NewVideoBook";
 import Title2 from "@/app/Components/Title2";
+import Hero from "@/app/Components/Hero";
 
 // Esta función se utiliza para generar las rutas estáticas de los slugs
 export function getStaticParams() {
@@ -34,10 +35,23 @@ export default function SlugPage({ params }) {
   }
 
   return (
-    <ContentSection>
-      <Title2 Class="pt-20 mb-3">{video.title}</Title2>
-      <LiteYouTubeEmbed id={video.yt} title={video.title} />
-      <NewVideoBook AspectRatio="aspect-[9/4]" />
-    </ContentSection>
+    <>
+      <video
+        className="h-svh  w-svw object-cover absolute"
+        src="video-hero.mp4"
+        autoPlay
+        playsInline
+        preload="auto"
+        loop
+        muted
+      />
+      <div className="absolute bottom-0 w-full h-[100%] bg-gradient-to-t from-[#000000] from-0% via-[#000000b1] via-40% to-[#000000] to-100%" />
+
+      <ContentSection Class="relative">
+        <Title2 Class="pt-20 mb-3">{video.title}</Title2>
+        <LiteYouTubeEmbed id={video.yt} title={video.title} />
+        <NewVideoBook AspectRatio="aspect-[9/4]" />
+      </ContentSection>
+    </>
   );
 }
