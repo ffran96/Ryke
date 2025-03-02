0685cc249b5aa573cb111f9bd81d68421f42a32a
Fran <=>
2025-03-02 20:54:31 +0100

    preload="none"

--- src/app/Components/Video.jsx
+++ src/app/Components/Video.jsx
@@ -9,7 +9,7 @@ export default function Video({Source, Class}) {
       muted
       playsInline
       loop
-      preload="auto"
+      preload="none"
       className={`${Class} w-[412px] object-cover rounded-[12px] cursor-pointer`}
     />
   );
